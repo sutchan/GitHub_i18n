0c94275d1d4db010a6a66f365886a59033f0a2de
Sut <xepinchan@qq.com>
2025-11-09 17:42:04 +0800

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.js$': 'babel-jest'
-  },
-  moduleFileExtensions: ['js'],
-  testEnvironment: 'node',
-  transformIgnorePatterns: ['/node_modules/']
-};
